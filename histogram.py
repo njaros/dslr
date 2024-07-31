37262d4b18eac4905445aa1e7580fa983b5cabb2
pouet <git@mesoule.tamere>
2024-07-31 18:57:23 +0200

    histogram

--- histogram.py
+++ histogram.py
@@ -14,6 +14,35 @@ def histogram(path):
                 or is to incorrect format, depending of pandas exceptions.
     """
     dataset = pd.read_csv(path)
+    dataset.drop(
+        columns=["Index", "First Name", "Last Name", "Birthday", "Best Hand"],
+        inplace=True,
+    )
+    houseSet = set()
+    col_one = dataset.columns[0]
+    for house in dataset[col_one]:
+        houseSet.add(house)
+    df_per_house: list[(str, pd.DataFrame)] = []
+    for house in houseSet:
+        df_per_house.append((house, dataset[dataset[col_one] == house]))
+    fig, axs = plt.subplots(
+        int((len(dataset.columns) - 1) / 4) + 1, 4, figsize=(20, 12)
+    )
+    fig.tight_layout()
+    fig.suptitle("comparisons between each house")
+    loop = 0
+    colors = ["red", "blue", "green", "yellow", "brown", "purple", "black"]
+    for col in dataset.columns[1:]:
+        x = int(loop / 4)
+        y = loop % 4
+        axs[x, y].set_title(col)
+        color_idx = 0
+        for df in df_per_house:
+            axs[x, y].hist(df[1][col], alpha=0.25, label=df[0], color=colors[color_idx])
+            color_idx += 1
+        loop += 1
+        axs[x, y].legend()
+    plt.savefig("histo.png")
 
 
 if __name__ == "__main__":
@@ -26,7 +55,7 @@ if __name__ == "__main__":
 
             rule : this program will returns an histogram comparing each
                    columns of a dataset between all possible values of the
-                   first column.            
+                   first column.
             """
         )
     elif ac != 2:
