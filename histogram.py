a88b18077843c47e398490e097ebc7245cfd2953
Clémence <clemence.cartet@geode.com>
2024-08-01 10:48:00 +0200

    hist

--- histogram.py
+++ histogram.py
@@ -9,10 +9,40 @@ def main():
     if len(sys.argv) != 2:
         print("Incorrect input.")
     else:
-        data = load(sys.argv[1])
-        if data is not None:
-            print(data.head(10))
-            # print(data.describe())
+        dataset = load(sys.argv[1])
+        if dataset is not None:
+            dataset.drop(columns="Index", inplace=True)
+            print(dataset.head(10))
+            grouped = dataset.groupby("Hogwarts House")
+            ravenclaw_data = grouped.get_group("Ravenclaw")
+            # print(ravenclaw_data)
+            slytherin_data = grouped.get_group("Slytherin")
+            gryffindor_data = grouped.get_group("Gryffindor")
+            hufflepuff_data = grouped.get_group("Hufflepuff")
+            _, axes = plt.subplots(nrows=2, ncols=7, figsize=(40, 4))
+            ravenclaw_data.hist(
+                ax=axes.ravel(),
+                alpha=0.5,
+                color="b",
+            )
+            slytherin_data.hist(
+                ax=axes.ravel(),
+                alpha=0.5,
+                color="g",
+            )
+            gryffindor_data.hist(
+                ax=axes.ravel(),
+                alpha=0.5,
+                color="r",
+            )
+            hufflepuff_data.hist(
+                ax=axes.ravel(),
+                alpha=0.5,
+                color="y",
+            )
+            plt.legend(labels=("Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"))
+            plt.tight_layout()
+            plt.show()
 
 
 if __name__ == "__main__":
