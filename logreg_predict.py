414fa938f524e44da1c66cdfa956e206997c0791
pouet <git@mesoule.tamere>
2024-08-12 16:23:03 +0200

    model_score.py

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,40 +1,59 @@
 import sys
 import pandas as pd
 import json
+import numpy as np
 from tools.logreg_config import FEATURE_TO_DROP
 
 
-def evaluate(value, theta0, theta1) -> float:
-    return value * theta1 + theta0
+def take_best_score(preds: dict, idx: int) -> str:
+    """function take_best_score
+    blablabla
+    """
+    keys = list(preds.keys())
+    best_score = preds[keys[0]][idx]
+    house = keys[0]
+    for key in keys[1:]:
+        if preds[key][idx] >= best_score:
+            best_score = preds[key][idx]
+            house = key
+    return house
 
 
-def is_member(line, params) -> bool:
-    """is_member function"""
-    value = (line[params["feature"]] - params["mean"]) / params["std"]
-    return evaluate(value, params["theta0"], params["theta1"]) >= 0
+def evaluate(X: np.ndarray, thetas: np.ndarray) -> np.ndarray:
+    """evaluate function
+    create the log(odds) value for each line of X
+    returns it as a np.ndarray
+    """
+    return X.dot(thetas)
 
 
-def predict_one(line, model) -> str:
-    """predict_one function"""
-    if is_member(line, model["Slytherin"]):
-        return "Slytherin"
-    if is_member(line, model["Ravenclaw"]):
-        return "Ravenclaw"
-    if is_member(line, model["Gryffindor"]):
-        return "Gryffindor"
-    return "Hufflepuff"
+def prepare_df_prediction(df: pd.DataFrame, scalers: dict) -> np.ndarray:
+    """prepare_df_prediction
+
+    blablabla
+    """
+    df.drop(columns=FEATURE_TO_DROP, inplace=True)
+    df.drop(columns="Hogwarts House", inplace=True)
+    for col in df.columns:
+        mean = scalers[col]["mean"]
+        std = scalers[col]["std"]
+        df[col] = df[col].map(lambda x: 0 if pd.isna(x) else (x - mean) / std)
+    datas = df.to_numpy()
+    biais = np.ones((datas.shape[0], 1), dtype=float)
+    X = np.hstack((datas, biais))
+    return X
 
 
-def predict(csv_path):
+def predict(df: pd.DataFrame, model: dict):
     """predict function"""
-    df = pd.read_csv(csv_path)
-    with open("model.json", "r") as io:
-        model = json.load(io)
-        io.close()
     result = pd.DataFrame(columns=["Index", "Hogwarts House"])
-    for index, line in df.iterrows():
-        result.loc[index] = [index, predict_one(line, model)]
-    result.to_csv("houses.csv", index=False)
+    X = prepare_df_prediction(df, model["features"])
+    predictions_per_house = {}
+    for key in model["thetas"]:
+        predictions_per_house[key] = evaluate(X, model["thetas"][key])
+    for i in range(len(X)):
+        result.loc[i] = [i, take_best_score(predictions_per_house, i)]
+    return result
 
 
 if __name__ == "__main__":
@@ -58,8 +77,12 @@ if __name__ == "__main__":
         )
     else:
         try:
-            predict(av[1])
+            df = pd.read_csv(av[1])
+            with open("model.json", "r") as io:
+                model = json.load(io)
+            predictions = predict(df, model)
+            predictions.to_csv("houses.csv", index=False)
         except UnicodeDecodeError as e:
             print(f"{e.__class__.__name__}: {e.args[4]}")
-        except Exception as e:
-            print(f"{e.__class__.__name__}: {e.args}")
+        # except Exception as e:
+        #     print(f"{e.__class__.__name__}: {e.args}")
