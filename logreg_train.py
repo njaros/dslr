ce0c1afa6e9d5bfa7dee6619ad5d308da7aee073
Clémence <clemence.cartet@geode.com>
2024-08-12 15:20:27 +0200

    comments

--- logreg_train.py
+++ logreg_train.py
@@ -7,67 +7,113 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from numpy import ndarray
 
 from utils import load, standardize
+from tools.logreg_config import FEATURES_TO_REMOVE
 
 
-FEATURES_TO_REMOVE = [
-    "Index",
-    "Arithmancy",
-    "Defense Against the Dark Arts",
-    "Divination",
-    "Muggle Studies",
-    "Ancient Runes",
-    "History of Magic",
-    "Transfiguration",
-    "Potions",
-    "Care of Magical Creatures",
-    "Charms",
-    "Flying",
-    # "Astronomy",
-    # "Herbology",
-]
+def sigmoid_function(
+    X: ndarray[float], thetas: ndarray[float]
+) -> tuple[ndarray[float], ndarray[float]]:
+    """Sigmoid function also called logistic function.
+    Transform results of linear function into probablities
+    of belonging to class 1, between 0 and 1.
 
+    Parameters
+    ----------
+    X: a matrix of features + biais,
+        its shape is m (nb of students) * n (nb of features
+        + 1 column for biais).
+    thetas: a vector of regression coefficients,
+        its size is (nb of features + 1).
 
-def sigmoid_function(X: np.ndarray, thetas: np.ndarray) -> np.ndarray:
-    """Sigmoid function that returns probabilities of an individual
-    being in a particular house"""
+    Returns
+    -------
+    probabilities: a vector of probabilities,
+        its size is (nb of students).
+    z: a vector corresponding to the results of the linear function,
+        its size is (nb of students).
+    """
     z = np.array(X.dot(thetas), dtype=float)
     probabilities = 1 / (1 + np.exp(-z))
-    # res = [1 if x > 0.5 else 0 for x in probabilities]
 
     return probabilities, z
 
 
-def cost_function(sigma: np.ndarray, target: np.ndarray) -> float:
-    """Log-likelihood : probability of observing the good results"""
-    return -sum(target * np.log(sigma) + (1 - target) * np.log(1 - sigma)) / target.size
+def cost_function(sigma: ndarray[float], target: ndarray[int]) -> float:
+    """Here we evaluate the performance of our model.
+    We calculate the likelihood: the plausibility of our model
+    in relation to real data, with the log loss function.
+
+    Parameters
+    ----------
+    sigma: a vector of probabilities,
+        its size is (nb of students).
+    target: a vector of our target 1 or 0 (1 is for class membership),
+        its size is (nb of students).
+
+    Returns
+    -------
+    res: product of all probabilities, using Bernoulli’s law.
+    """
+    res = -sum(target * np.log(sigma) + (1 - target) * np.log(1 - sigma)) / target.size
+
+    return res
 
 
-def find_gradients(X: np.ndarray, sigma: np.ndarray, target: np.ndarray) -> np.ndarray:
-    """Find gradients with partial differential of thetas"""
+def find_gradients(
+    X: ndarray[float], sigma: ndarray[float], target: ndarray[int]
+) -> ndarray[float]:
+    """Calcul of gradients (or partial derivatives) of the cost function.
+
+    Parameters
+    ----------
+    X: a matrix of features + biais,
+        its shape is m (nb of students) * n (nb of features
+        + 1 column for biais).
+    sigma: a vector of probabilities,
+        its size is (nb of students).
+    target: a vector of our target 1 or 0 (1 is for class membership),
+        its size is (nb of students).
+
+    Returns
+    -------
+    gradients: a vector of (nb of features + 1),
+        a gradient per theta.
+    """
     X_t = np.transpose(X)  # ndarray(n-features + 1, m)
-    return X_t.dot(np.subtract(sigma, target)) / target.size
+    gradients = X_t.dot(np.subtract(sigma, target)) / target.size
+
+    return gradients
 
 
-def training(features: np.ndarray, target: np.ndarray):
-    """This function will find the most optimized thetas to minimize cost function"""
-    nb_iter = 100
-    learning_rate = 3.0
+def training(
+    features: ndarray[float], target: ndarray[int], epochs: int, learning_rate: float
+):
+    """Our learning algorithm, using gradient descent.
+    We will find the most optimized thetas to minimize the cost function.
+
+    Parameters
+    ----------
+    features: a vector of size (nb of features).
+    target: a vector of our target 1 or 0 (1 is for class membership),
+        its size is (nb of students).
+    epochs: nb of one complete pass of the training data set through our training algorithm.
+    learning_rate: a hyper-parameter used to govern the pace at which
+    our algorithm updates thetas.
+    """
     cost_history: list[float] = []
 
     m = features.shape[0]
-    biais = np.ones((m, 1))
-    X = np.hstack((features, biais))  # ndarray(m, n-features + 1)
+    X = np.hstack((features, np.ones((m, 1))))  # ndarray(m, n-features + 1)
     thetas = np.zeros(X.shape[1])  # ndarray(n-features + 1)
 
-    for i in range(nb_iter):
-        proba, z = sigmoid_function(X, thetas)  # ndarray(m)
+    for i in range(epochs):
+        proba, z = sigmoid_function(X, thetas)
         cost_history.append(cost_function(proba, target))
-        gradients = find_gradients(X, proba, target)  # ndarray(n-features + 1)
-        thetas = np.subtract(
-            thetas, (learning_rate * gradients)
-        )  # ndarray(n-features + 1)
+        gradients = find_gradients(X, proba, target)
+        thetas = np.subtract(thetas, (learning_rate * gradients))
 
     _, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 5))
     axes[0].scatter(z, target, color="b", s=5, alpha=0.4)
@@ -80,7 +126,7 @@ def training(features: np.ndarray, target: np.ndarray):
 
 
 def get_ready(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
-    """Prepare dataset for training : drop unwanted features,
+    """Prepare dataset for training with multi-factor logistic regression: drop unwanted features,
     drop nan values, standardize data
     Return a DataFrame with our selected features,
     and a DataFrame for houses: each column represents one house (with 1 and 0)"""
@@ -105,6 +151,8 @@ def get_ready(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
 
 def main():
     """Main function"""
+    epochs = 100
+    learning_rate = 3.0
     if len(sys.argv) != 2:
         print("Incorrect input.")
     else:
@@ -114,7 +162,9 @@ def main():
             df, houses = get_ready(dataset)
             weights = {}
             for col in houses.columns:
-                thetas = training(df.to_numpy(), houses[col].to_numpy())
+                thetas = training(
+                    df.to_numpy(), houses[col].to_numpy(), nb_iter, learning_rate
+                )
                 weights[col] = thetas
 
             with open("weigths.json", "w", encoding="utf8") as file:
