0478cb896009780de5a513da672ff2fc4434c1f6
pouet <git@mesoule.tamere>
2024-08-16 16:06:58 +0200

    choose algorithm with config file

--- logreg_train.py
+++ logreg_train.py
@@ -61,7 +61,10 @@ def cost_function(sigma: ndarray[float], target: ndarray[int]) -> float:
     -------
     res: product of all probabilities, using Bernoulli’s law.
     """
-    res = -sum([np.log(x) if y == 1 else np.log(1 - x) for x, y in zip(sigma, target)])
+    res = (
+        -sum([np.log(x) if y == 1 else np.log(1 - x) for x, y in zip(sigma, target)])
+        / target.size
+    )
     # res = -sum(target * np.log(sigma) + (1 - target) * np.log(1 - sigma)) / target.size
 
     return res
